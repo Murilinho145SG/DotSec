d1b05ab17fdad676c470888297690116b746af33
Murilinho145SG <bye456oi@gmail.com>
2024-02-18 20:28:43 -0300

    2 types

--- src/DotSec.java
+++ src/DotSec.java
@@ -2,8 +2,5 @@ import sec.SecCommon;
 import sec.SecEvent;
 
 public class DotSec {
-    public static void main(String[] args) {
-        SecEvent.read(SecCommon.getPath(".sec"));
-        System.out.println(SecEvent.get("ALELO"));
-    }
+
 }
